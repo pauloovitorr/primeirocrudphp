1e96cbe549f743a7ea1292f9559ff16f70a2aa8e
pauloovitorr <paulov.pv25@gmail.com>
2026-02-19 21:02:43 -0300

    Adicionei o readme e um exemplo de como deve ser o banco de dados

--- conexao.php
+++ conexao.php
@@ -8,13 +8,5 @@ $senha = '';
 
 $conecte = new mysqli($host,$usuario,$senha,$db);
 
-//if($conecte->connect_error)
-// {
-//     echo "Deu ruim: " . $conecte->connect_errno;
-// }
-// else
-// {
-//     echo 'Deu bom';
-// }
 
 ?>
